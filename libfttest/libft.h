9ecda055eaeda8523b82ed05b3056cb8b48bd350
Strafqw <sampaio2150@gmail.com>
2025-11-10 15:17:49 +0000

    pum pum

--- libfttest/libft.h
+++ libfttest/libft.h
@@ -6,7 +6,7 @@
 /*   By: joasampa <joasampa@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 20:47:44 by joasampa          #+#    #+#             */
-/*   Updated: 2025/11/06 12:36:44 by joasampa         ###   ########.fr       */
+/*   Updated: 2025/11/10 15:17:30 by joasampa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_striter(char *s, void (*f)(unsigned int, char*));
 
 /* ---------------   FILE DESCRIPTORS   --------------- */
 void	ft_putchar_fd(char c, int fd);
